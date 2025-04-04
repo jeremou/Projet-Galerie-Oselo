67c4d51420936d7ecc8ef4c709ba145eaea16c5e
jeremou <jeremie.mourinet@gmail.com>
2025-04-04 10:04:52 +0200

    changement de langues

--- config/database.php
+++ config/database.php
@@ -1,23 +1,24 @@
 <?php
 class Database {
-    private $host = 'localhost';
-    private $db_name = 'galerie_oselo';
-    private $username = 'root';
-    private $password = '';
-    private $conn;
-
-    // Connexion à la base de données
+    // Database credentials
+    private $host = "localhost";
+    private $db_name = "galerie_oselo";
+    private $username = "root";
+    private $password = "";
+    public $conn;
+ 
+    // Get the database connection
     public function getConnection() {
         $this->conn = null;
-
+ 
         try {
-            $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password);
-            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
             $this->conn->exec("set names utf8");
-        } catch(PDOException $e) {
-            echo 'Erreur de connexion: ' . $e->getMessage();
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $exception) {
+            echo "Connection error: " . $exception->getMessage();
         }
-
+ 
         return $this->conn;
     }
 }
